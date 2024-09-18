b82f7efea614a52bd8abec13ceb4a92299a38653
phuonghoa-hndh <79041042+phuonghoa-hndh@users.noreply.github.com>
2024-09-18 16:33:07 +0700

    Update Item.java

--- src/FinalDSA/Item.java
+++ src/FinalDSA/Item.java
@@ -1,19 +1,10 @@
 package FinalDSA;
 
 
-/**
- * Represents an Item that can be placed in a Knapsack
- * @author Rory Hackney
- */
 public class Item implements Comparable<Item> { // Added comparable needed for Greedy Algo, compared by value/weight
     public final int benefit;
     private final int weight;
 
-    /**
-     * Creates an Item
-     * @param benefit benefit of the Item; must be at least 1
-     * @param weight weight of the Item; must be at least 1
-     */
     public Item(int benefit, int weight){
         if (benefit < 1) throw new IllegalArgumentException("benefit must be at least 1");
         if (weight < 1) throw new IllegalArgumentException("weight must be at least 1");
